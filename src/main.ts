025dd485a4a16c45382f70831529e97fe85fa8a9
pepsi-dsf <1204182164@qq.com>
2021-11-04 15:29:20 +0800

    update my site

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ axios.defaults.baseURL = 'http://apis.imooc.com/api/'
 // 下面的 icode 值是从慕课网获取的 token 值，可以在课程右侧的项目接口校验码找到
 axios.interceptors.request.use(config => {
   // ... 其他代码
-  // get 请求，添加到 url 中
+  // get 请求，添加到 url 中。
   config.params = { ...config.params, icode: '0EE6DDEF17AA0C0B' }
   // 其他请求，添加到 body 中
   // 如果是上传文件，添加到 FormData 中
